c730dbdf3c2d59ca1aed98f192bc2f9a29ad4e36
Beatriz Barros <work.beatrizbarros@gmail.com>
2024-11-22 22:18:58 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from "express";
+import routes from "./src/routes/postsRoutes.js";
+// Importa o framework Express para criar a aplicação web
+
+
 
 const app = express();
+routes(app);
+// Cria uma instância do Express, que será o núcleo da aplicação web.
+
 app.listen(3000, () => {
-    console.log("Servidor escutando...");
+  console.log("Servidor escutando...");
+  // Inicia o servidor na porta 3000 e exibe uma mensagem no console quando o servidor estiver pronto para receber requisições.
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Boas vindas a imersão!");
-});
